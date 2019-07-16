272f5ae18d855c1f3e10cd41539e3b470ca55146
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-16 18:10:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,9 +4,9 @@ attr_accessor :total, :discount, :items, :last_transaction
 
   def initialize(discount = 0)
     @total = 0
-    @items = []
     @discount = discount
-    @last_transaction
+    @items = []
+    @last_transactions
   end
 
   def add_item(title, price, number = 1)
