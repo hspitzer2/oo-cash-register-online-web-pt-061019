233f826d0fdc1f35404d104206c6761bbb4a4ec5
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-16 17:54:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,6 +4,7 @@ attr_accessor :total, :discount, :items
 
   def initialize(discount = nil)
     @total = 0
+    @items
     @discount = discount
 
   end
@@ -11,9 +12,13 @@ attr_accessor :total, :discount, :items
   def add_item(title, price, number = 1)
     @total +=(price * quantity)
     apply_discount
+    quantity.times do @items<<title
+
   end
 
   def apply_discount
+    @total = total
+
 
   end
 
